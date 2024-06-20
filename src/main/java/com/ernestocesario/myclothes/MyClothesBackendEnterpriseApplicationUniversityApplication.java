c1b4cde6d09c1a90c9b83ceeda33adaa0c56a582
ernestocesario <cesarioernesto20@gmail.com>
2024-06-20 17:13:25 +0200

    Initial commit

---
+++ src/main/java/com/ernestocesario/myclothes/MyClothesBackendEnterpriseApplicationUniversityApplication.java
@@ -0,0 +1,12 @@
+package com.ernestocesario.myclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClothesBackendEnterpriseApplicationUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyClothesBackendEnterpriseApplicationUniversityApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ernestocesario/myclothes/MyClothesBackendEnterpriseApplicationUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernestocesario.myclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClothesBackendEnterpriseApplicationUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
